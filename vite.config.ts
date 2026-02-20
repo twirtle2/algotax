9874835779975bd232942389fd6fa9db1d5fd8d2
twirtle2 <twirtle2@users.noreply.github.com>
2026-02-20 11:57:54 +1100

    fix: update repository base url to algotax

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/algo-tax/',
+    base: '/algotax/',
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
